1d937d64ae74be7d9c89e178dac64573538feb02
mokutsu <misato.okutsu@gmail.com>
2018-07-02 19:12:51 -0400

    section one done!

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,12 +8,20 @@ class SearchBar extends React.Component {
     // calls parent class, Component's method (essentially super(class).__init__() i think)
     super(props);
     // the constructor function is probably the only time we will manipulate state directly
-    this.state = { term: '' };
+    this.state = { term: 'default starting value' };
   }
 
   render() {
     // we set the state by using the setState function instead
-    return  <input onChange={event => this.setState({ term: event.target.value })} />
+    return (
+      <div>
+        <input
+          value={this.state.term} // setting value here makes it a controlled element, the value isn't automatically updated. the user types, which causes the onchange to fire, which changes state, which then changes state, which then changes value. without this line, the value would have just updated.
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+        value of the input: {this.state.term}
+      </div>
+    )
   }
 
 }
