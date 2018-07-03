cabbfbecb741d93aa76c7f46ad66740b8e296c30
mokutsu <misato.okutsu@gmail.com>
2018-07-02 20:39:37 -0400

    add call to yt search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAuCg2VIhNBsSmh6AsHSRKHuZnHoA9vZ9s';
+
+YTSearch({key: API_KEY, term: 'windsurfing'}, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
