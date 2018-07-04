05cd23d2aba7fa233d34a4606a72548b66cc0446
mokutsu <misato.okutsu@gmail.com>
2018-07-03 23:16:25 -0400

    add containers directory for smart components

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+// course notes prepending filename title with the file type
+// step 1, create function. step 2, wire up into application. 
+export default function() {
+  return [
+    {title: 'JavaScript: The Good Parts'},
+    {title: 'Harry Potter and the Philosopher\'s stone'},
+    {title: 'Eloquent JavaScript'},
+    {title: 'the Phoenix project'}
+  ]
+}
