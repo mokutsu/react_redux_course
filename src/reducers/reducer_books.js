64ec9f4f3aeac8221679f9e9ff3e2f98e93a68c3
mokutsu <misato.okutsu@gmail.com>
2018-07-10 22:50:36 -0400

    adding in action and reducer for updating book actions

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,10 +1,10 @@
 // course notes prepending filename title with the file type
-// step 1, create function. step 2, wire up into application. 
+// step 1, create function. step 2, wire up into application.
 export default function() {
   return [
-    {title: 'JavaScript: The Good Parts'},
-    {title: 'Harry Potter and the Philosopher\'s stone'},
-    {title: 'Eloquent JavaScript'},
-    {title: 'the Phoenix project'}
+    {title: 'JavaScript: The Good Parts', pages: 103},
+    {title: 'Harry Potter and the Philosopher\'s stone', pages: 113},
+    {title: 'Eloquent JavaScript', pages: 103},
+    {title: 'the Phoenix project', pages: 13}
   ]
 }
