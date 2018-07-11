8c47b60ce5d14f231f4f8fceee25a19505f99e87
mokutsu <misato.okutsu@gmail.com>
2018-07-10 22:35:54 -0400

    partway

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
+import BookDetail from '../containers/book-detail';
 import BookList from '../containers/book-list'
 
 export default class App extends Component {
   render() {
     return (
-      <BookList />
+      <div>
+        <BookList />
+        <BookDetail />
+      </div>
+
     );
   }
 }
