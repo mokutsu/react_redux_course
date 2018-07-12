27c1df7b97c28080e58013b93940a20f80208b92
mokutsu <misato.okutsu@gmail.com>
2018-07-11 22:57:49 -0400

    fixup

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import BookDetail from '../containers/book-detail';
 import BookList from '../containers/book-list'
-
+// RETURN 2 COMPONENTS, BOOK LIST SAND BOOK DETAIL
 export default class App extends Component {
   render() {
     return (
