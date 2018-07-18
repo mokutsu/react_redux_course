d4d8d7712ee2f3687b8a88878f2a7345563213c1
mokutsu <misato.okutsu@gmail.com>
2018-07-17 20:12:03 -0400

    fix up npm version

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
 const API_KEY = "2de69c4eb779758c10387a5ac9054680";
+const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(cityName) => {
+  const url = `${ROOT_URL}&q=${city},`
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}
