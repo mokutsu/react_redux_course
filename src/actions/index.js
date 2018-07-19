fb6f0f7e66c8834c98adc25cd06a16690b4befe8
mokutsu <misato.okutsu@gmail.com>
2018-07-19 00:05:16 -0400

    add note

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ const API_KEY = "2de69c4eb779758c10387a5ac9054680";
 const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
-
+// this function fetches weather from the open weather map API and returns the payload using axios
 export function fetchWeather(cityName) => {
   const url = `${ROOT_URL}&q=${city},`
   const request = axios.get(url);
