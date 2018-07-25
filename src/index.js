70ea50709946cc55bf21be1b7704a52f9954c08b
mokutsu <misato.okutsu@gmail.com>
2018-07-25 07:39:24 -0400

    fix reference error

--- src/index.js
+++ src/index.js
@@ -2,15 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import ReduxPromise from 'redux-promise';
+
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+// create 2 seperate components for testing
+
+class Hello extends React.Component {
+  render() { return <div>hello</div> }
+}
+
+class Farewell extends React.Component {
+  render() { return <div>Farewell</div> }
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      {// path = string for the path url
+      }
+      <div>
+        navigation header here
+        <Route path="/hello" component={Hello} />
+        <Route path="/farewell" component={Farewell} />
+        <Route />
+      </div>
+
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
