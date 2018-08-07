fa29c7fcadb0fa6bee05bd2e31a7857ae3e8bf8d
mokutsu <misato.okutsu@gmail.com>
2018-08-06 23:26:47 -0400

    redux field step 1 working

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
-  render() {
+  renderTitleField(field) { // field object contains event handlers, field.input contains a bunnch of event handlers. By doing ...field.input, just says all teh properties within field.input are spread out and assigned as props on the input element
     return (
       <div>
-        posts new!
+        <input
+          type="text"
+          {...field.input}
+        />
       </div>
+    )
+  }
+  render() {
+    return (
+      <form>
+        <Field
+          name='post-title'
+          component={this.renderTitleField}
+        />
+
+      </form>
     );
   }
 }
 
-export default PostsNew;
+// pass in function with form property (name of form), just make sure it is unique to keep the different forms seperate
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
